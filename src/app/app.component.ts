a8a0eaffb5f4364e43477517e0a277f1da29c64f
Borges <borgespaulo72@yahoo.com.br>
2026-04-21 13:57:23 -0300

    feat: modal de tarefas criado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { TaskFormModalComponent } from './components/task-form-modal/task-form-modal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, MainContentComponent, FooterComponent],
+  imports: [
+    HeaderComponent,
+    MainContentComponent,
+    FooterComponent,
+    TaskFormModalComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
